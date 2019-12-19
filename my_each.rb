48e5edaa601c498a75939e3576240ac8e6a4e78c
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 04:28:17 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  index = 0
+  while index < array.length # 3
+    yield(array[index])
+    # index += 1
+    index += 1
+  end
+  return array
+end
+
+my_each([0,1,2,3,4]) do |number|
+  number * 2
 end
