0d24cb223b939a23273a826a320174e3e3be7cd8
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 04:26:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 def my_each # put argument(s) here
   # code here
-index = 0
-while index 
-
-
+  index = 0
+  while index < array.length # 3
+    yield(array[index])
+    # index += 1
+    index += 1
+  end
+  return array
 end
